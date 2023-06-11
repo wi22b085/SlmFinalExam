6989becdd24806f3a89a774719b0541a6b300eb4
Alexander Hickelsberger <wi22b085@technikum-wien.at>
2023-06-11 18:48:45 +0200

    first commit

---
+++ src/test/java/com/example/slmfinalexam/SlmFinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmfinalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmFinalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
